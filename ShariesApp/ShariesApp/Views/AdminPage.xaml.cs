f0ee0d6f2c90e1e67f4f24ef8c761e9beadeac27
Corban10 <Corban30303@hotmail.com>
2018-11-03 23:24:31 +1300

    admin page 2
    can change details now but account number method may need to be changed,
    azure easy tables have limitations

--- ShariesApp/ShariesApp/Views/AdminPage.xaml.cs
+++ ShariesApp/ShariesApp/Views/AdminPage.xaml.cs
@@ -29,59 +29,96 @@ namespace ShariesApp.Views
         }
         private void setLimitButtonClicked(object sender, EventArgs e)
         {
-            if (clSelectedIndex >= 0)
+            if (clSelectedIndex >= 0) // if picker selection valid
             {
-                var newSystemData = App.Database.GetSystemData("1");
-                switch (clSelectedIndex)
+                var newSystemData = App.Database.GetSystemData("1"); // get system data
+                if (checkIsConvertableToDouble(setLimitEntry.Text)) // check if valid value
                 {
-                    case 0:
-                        newSystemData.creditLimit = checkIsConvertableToDouble(setLimitEntry.Text) ? 
-                            Convert.ToDouble(setLimitEntry.Text) : 
-                            newSystemData.creditLimit;
-                        break;
-                    case 1:
-                        newSystemData.textLimit = checkIsConvertableToDouble(setLimitEntry.Text) ?
-                            Convert.ToDouble(setLimitEntry.Text) :
-                            newSystemData.textLimit;
-                        break;
-                    case 2:
-                        newSystemData.dataLimit = checkIsConvertableToDouble(setLimitEntry.Text) ?
-                            Convert.ToDouble(setLimitEntry.Text) :
-                            newSystemData.dataLimit;
-                        break;
-                    case 3:
-                        newSystemData.minutesLimit = checkIsConvertableToDouble(setLimitEntry.Text) ?
-                            Convert.ToDouble(setLimitEntry.Text) :
-                            newSystemData.minutesLimit;
-                        break;
+                    switch (clSelectedIndex) // picker value selection
+                    {
+                        case 0:
+                            newSystemData.creditLimit = Convert.ToDouble(setLimitEntry.Text); 
+                            break;
+                        case 1:
+                            newSystemData.textLimit = Convert.ToDouble(setLimitEntry.Text);
+                            break;
+                        case 2:
+                            newSystemData.dataLimit = Convert.ToDouble(setLimitEntry.Text);
+                            break;
+                        case 3:
+                            newSystemData.minutesLimit = Convert.ToDouble(setLimitEntry.Text);
+                            break;
+                    }
+                    App.Database.UpdateSystemDataAsync(newSystemData); //update row
+                    nameLabel.Text = string.Format("Credit limit: {0}\nText limit: {1}\nData limit: {2}\nMinutes limit: {3}\n",
+                        newSystemData.creditLimit,
+                        newSystemData.textLimit,
+                        newSystemData.dataLimit,
+                        newSystemData.minutesLimit
+                        ); //display new value(s) to label
                 }
-                App.Database.UpdateSystemDataAsync(newSystemData);
-                nameLabel.Text = string.Format("Credit limit: {0}\nText limit: {1}\nData limit: {2}\nMinutes limit: {3}\n",
-                    newSystemData.creditLimit,
-                    newSystemData.textLimit,
-                    newSystemData.dataLimit,
-                    newSystemData.minutesLimit
-                    );
-                setLimitEntry.Text = "";
+                else
+                {
+                    nameLabel.Text = "Invalid value";
+                }
             }
+            setLimitEntry.Text = "";
         }
         private bool checkIsConvertableToDouble(string input)
         {
             return (!string.IsNullOrWhiteSpace(input) && Double.TryParse(input, out double e));
         }
+        private bool checkIsConvertableToInt(string input)
+        {
+            return (!string.IsNullOrWhiteSpace(input) && Int32.TryParse(input, out int e));
+        }
         private void cDetailsSelectedIndexChanged(object sender, EventArgs e)
         {
             cDetailsPicker = (Picker)sender;
             cDetailsSelectedIndex = cDetailsPicker.SelectedIndex;
+            switch (cDetailsSelectedIndex)
+            {
+                case 0:
+                    newDetails.Placeholder = "New Account Number";
+                    break;
+                case 1:
+                    newDetails.Placeholder = "New Password";
+                    break;
+            }
         }
         private void changeDetailsButtonClicked(object sender, EventArgs e)
         {
-            if (clSelectedIndex >= 0)
+            if (cDetailsSelectedIndex >= 0 && checkIsConvertableToInt(accountNumber.Text)) //check if account number is number
             {
-                // check exists
-
-                // change to new
-                // nameLabelTwo.Text = string.Format("{0} {1}, {2}", cDetailsPicker.Items[cDetailsSelectedIndex], oldDetails.Text, newDetails.Text);
+                var getUserData = App.Database.GetUserData(accountNumber.Text); //get this users details
+                if (!string.IsNullOrWhiteSpace(getUserData.id)) //check if returned value is not blank object
+                {
+                    switch (cDetailsSelectedIndex) //picker value, 0 = change account number, 1 = change password
+                    {
+                        case 0:
+                            // cannot change back to the original ac number because azure never perma deletes IDs
+                            if (checkIsConvertableToInt(newDetails.Text)) // check if new account number is valid
+                            {
+                                App.Database.DeleteUserDataAsync(getUserData); //delete old row
+                                getUserData.id = newDetails.Text; // change account number
+                                App.Database.InsertUserDataAsync(getUserData); //insert new row
+                            }
+                            break;
+                        case 1:
+                            if (!string.IsNullOrWhiteSpace(newDetails.Text)) // check if password is valid
+                            {
+                                getUserData.Password = newDetails.Text;
+                                App.Database.UpdateUserDataAsync(getUserData); //update row
+                            }
+                            break;
+                    }
+                    //display user values for confirmation
+                    nameLabelTwo.Text = string.Format("Account number: {0}\nPassword: {1}", getUserData.id, getUserData.Password); 
+                }
+            }
+            else
+            {
+                nameLabelTwo.Text = "Invalid account number";
             }
         }
     }
