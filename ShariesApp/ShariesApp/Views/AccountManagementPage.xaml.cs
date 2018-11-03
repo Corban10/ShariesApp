9e9b527363fccf20a8908d70831dcd525a4293ee
Corban10 <Corban30303@hotmail.com>
2018-11-04 03:00:07 +1300

    management page 3
    block account finished

--- ShariesApp/ShariesApp/Views/AccountManagementPage.xaml.cs
+++ ShariesApp/ShariesApp/Views/AccountManagementPage.xaml.cs
@@ -19,18 +19,16 @@ namespace ShariesApp.Views
         private void blockAccountButtonClicked(object sender, EventArgs e)
         {
             blockAccountLabel.Text = "";
-            if (Int32.TryParse(blockAccountEntry.Text, out int test))
+            if (Int32.TryParse(blockAccountEntry.Text, out int test)) // check if entry value is valid
             {
+                // check if blocked // make this a method maybe
                 var blockedAccountsList = App.Database.QueryBlockedAccountsByBlocker(Convert.ToInt32(MainPage.loggedInUser.accountNumber));
                 bool blockeeAlreadyExists = false;
-                foreach (var item in blockedAccountsList)
-                {
+                foreach (var item in blockedAccountsList) 
                     if (item.blockee == Convert.ToInt32(blockAccountEntry.Text))
-                    {
                         blockeeAlreadyExists = true;
-                    }
-                }
-                if (!blockeeAlreadyExists)
+                // if it doesnt, then block
+                if (!blockeeAlreadyExists) 
                 {
                     var blockedAccountObject = new BlockedAccounts
                     {
@@ -38,19 +36,24 @@ namespace ShariesApp.Views
                         blockee = Convert.ToInt32(blockAccountEntry.Text)
                     };
                     App.Database.InsertBlockedAccountsAsync(blockedAccountObject);
-
                     blockAccountLabel.Text += "Blocked accounts:\n";
                     foreach (var item in blockedAccountsList)
-                    {
                         blockAccountLabel.Text += string.Format("{0}\n", item.blockee);
-                    }
                     blockAccountLabel.Text += string.Format("{0}\n", blockAccountEntry.Text);
+                    blockAccountLabel.Text = "Account blocked successfully";
                 }
+                else
+                    blockAccountLabel.Text = "Account is already blocked";
             }
-            else
-            {
-                blockAccountLabel.Text = "Invalid Account number";
-            }
+            blockAccountEntry.Text = "";
+        }
+        private void unblockAccountButtonClicked(object sender, EventArgs e)
+        {
+
+        }
+        private void changePasswordButtonClicked(object sender, EventArgs e)
+        {
+
         }
     }
 }
