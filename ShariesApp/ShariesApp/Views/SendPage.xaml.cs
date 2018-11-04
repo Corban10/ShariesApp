f67cac375290210bd9b870ecc719532a2b85aa00
Corban10 <Corban30303@hotmail.com>
2018-11-04 21:05:38 +1300

    Send page layout

--- ShariesApp/ShariesApp/Views/SendPage.xaml.cs
+++ ShariesApp/ShariesApp/Views/SendPage.xaml.cs
@@ -11,10 +11,40 @@ namespace ShariesApp.Views
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class SendPage : ContentPage
-	{
-		public SendPage ()
+    {
+        private static Picker senderPicker;
+        private static int senderSelectedIndex;
+
+        public SendPage ()
 		{
 			InitializeComponent ();
-		}
-	}
+        }
+        private void SendButtonClicked(object sender, EventArgs e)
+        {
+
+        }
+        private void SenderPickerSelectedIndexChanged(object sender, EventArgs e)
+        {
+            senderPicker = (Picker)sender;
+            senderSelectedIndex = senderPicker.SelectedIndex;
+            if (senderSelectedIndex >= 0) // if picker selection valid
+            {
+                switch (senderSelectedIndex) // picker value selection
+                {
+                    case 0:
+                        sendAmountEntry.Placeholder = "Dollars";
+                        break;
+                    case 1:
+                        sendAmountEntry.Placeholder = "Texts";
+                        break;
+                    case 2:
+                        sendAmountEntry.Placeholder = "Megabytes";
+                        break;
+                    case 3:
+                        sendAmountEntry.Placeholder = "Minutes";
+                        break;
+                }
+            }
+        }
+    }
 }
